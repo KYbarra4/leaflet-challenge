43dc99e66a0a4fd7c454930e14da7e694e78307a
Kevin Ybarra <kevin.ybarra@gmail.com>
2023-01-22 20:15:01 -0600

    images

--- Leaflet-Part-2/static/js/logic.js
+++ Leaflet-Part-2/static/js/logic.js
@@ -1,6 +1,8 @@
+// Creating layer groups to place on top of the maps
 let earthquake = L.layerGroup();
 let tecPlates = L.layerGroup();
 
+// Creating tile layers for the map visual
 let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
@@ -9,6 +11,7 @@ let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
+// Techtonic Plates geoData
 let platesgeo = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json';
 
 d3.json(platesgeo).then(data2 => {
@@ -19,20 +22,23 @@ d3.json(platesgeo).then(data2 => {
   tecPlates.addTo(myMap);
 });
 
+// Creating basemaps for base layers
 let baseMaps = {
   'Street': street,
   'Topo': topo,
 };
 
+// Creating overlays to hold overlay objects
 let overlayMap = {
   'Earthquakes': earthquake,
   'Tectonic Plates': tecPlates
 };
 
+// Creating a map on page load
 let myMap = L.map("map", {
   center: [37.7749, -100.4194],
   zoom: 4.5,
-  layers: [street, topo]
+  layers: [street, tecPlates]
 });
 
 L.control.layers(baseMaps, overlayMap, {
@@ -44,10 +50,12 @@ let geoData = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_wee
 d3.json(geoData).then(data => {
   console.log(data);
 
+// Creating markers and inlarging for better visual
   function createMarker(magnitude) {
     return magnitude * 5; 
   }
 
+// Giving colors to various earthquake depths
     function createColor(depth) {
       switch(true) {
         case depth >= 90:
@@ -66,6 +74,7 @@ d3.json(geoData).then(data => {
       }
     }
 
+// Creating features that give data about the given earthquake
     L.geoJSON(data, {
       pointToLayer: function (feature, mapCoordinate) {
         return L.circleMarker(mapCoordinate, 
@@ -77,6 +86,8 @@ d3.json(geoData).then(data => {
             weight: .5
           });
       },
+
+// Popup
       onEachFeature: function (feature, layers) {
         layers.bindPopup('<h2>Magnitude: ' + feature.properties.mag + '</h2><hr><h3>Location: '+ feature.properties.place + '</h3><hr><h4>Date: '
         + new Date(feature.properties.time) + '</h4>');
@@ -85,6 +96,7 @@ d3.json(geoData).then(data => {
     }).addTo(earthquake);
     earthquake.addTo(myMap);
 
+// Creating a legend and pinning it to the bottom right
     let legend = L.control({position: 'bottomright'});
     legend.onAdd = function() {
       div = L.DomUtil.create('div', 'info legend'),
@@ -94,6 +106,7 @@ d3.json(geoData).then(data => {
 
       div.innerHTML = legendHead
 
+// Inseting threshold into legend and styling
     for (let i = 0; i < depth.length; i++) {
       labels.push('<ul style="background-color:' + createColor(depth[i] + 1) + '"> <span>' + depth[i] + 
       (depth[i + 1] ? '&ndash;' + depth[i + 1] + '' : '+') + '</span></ul>');
